2d35ebc0c30039aa68065529ba16e4f1170d7c1e
Mike <mike.d.meier@gmail.com>
2013-04-20 07:41:25 -0600

    v2.1 upgrade

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,62 +2,85 @@
 PLUGIN_PREFIX = "/video/IMDBTrailers"
 
 TRAILERS     = "http://www.imdb.com/features/video/trailers"
-CONTENT_URL  = "http://www.imdb.com/video/trailers/data/_ajax/adapter/shoveler?list=%s&caller_name=ava_video_trailers" #"http://www.imdb.com/video/trailers/data/_json?list=%s"
+CONTENT_URL  = "http://www.imdb.com/video/trailers/data/_ajax/adapter/shoveler?list=%s&caller_name=ava_video_trailers"
 DETAILS_PAGE = "http://www.imdb.com/video/imdb/%s/html5"
-
-SORT_POPULAR = "popular"
-SORT_RECENT  = "recent"
-
-SORT_PREFS_KEY = "sortOrder"
-
-CACHE_TIME = 3600
-
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "IMDb HD Trailers", "icon-default.png", "art-default.jpg")
-  Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
-  Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
-  MediaContainer.title1 = L('IMDb HD Trailers')
-  MediaContainer.art = R('art-default.jpg')
-  DirectoryItem.thumb = R('icon-default.png')
-  HTTP.CacheTime = CACHE_TIME
-
+  ObjectContainer.title1 = L('IMDb HD Trailers')
+  ObjectContainer.art = R('art-default.jpg')
+  DirectoryObject.thumb = R('icon-default.png')
+  
 ####################################################################################################
+@handler(PLUGIN_PREFIX, "IMDb HD Trailers", "icon-default.png", "art-default.jpg")
 def MainMenu():
-    dir = MediaContainer(viewGroup='List')
-    dir.Append(Function(DirectoryItem(HDVideos, title="Recent HD Trailers"), sort="recent"))
-    dir.Append(Function(DirectoryItem(HDVideos, title="Popular HD Trailers"), sort="top_hd"))
-    dir.Append(Function(DirectoryItem(HDVideos, title="Popular Movies"), sort="popular"))
-    return dir
+  oc = ObjectContainer()
+  oc.add(DirectoryObject(key=Callback(HDVideos, sort="recent", title="Recent HD Trailers"), title="Recent HD Trailers"))
+  oc.add(DirectoryObject(key=Callback(HDVideos, sort="top_hd", title="Popular HD Trailers"), title="Popular HD Trailers"))
+  oc.add(DirectoryObject(key=Callback(HDVideos, sort="popular", title="Popular Movies"), title="Popular Movies"))
+  return oc
 
 ####################################################################################################
-def HDVideos(sender, sort):
-  dir = MediaContainer(viewGroup='Details')
+def HDVideos(sort, title):
+  oc = ObjectContainer(title2=title)
   contentUrl = CONTENT_URL % sort
   content = JSON.ObjectFromURL(contentUrl)
   for video in content['model']['items']:
     videoId = video['video']['videoId']
-    thumb = video['video']['slateUrl']
+    thumb = video['display']['poster']['url']
+    try:
+      thumb = thumb.replace('_SY209_CR0,0,141,209_', '_SY836_CR0,0,564,836_')
+      thumb = thumb.replace('_SX141_CR0,0,141,209_', '_SX564_CR0,0,564,836_')
+    except:
+      thumb = video['video']['slateUrl']
     title = video['display']['text'] + ' - ' + video['video']['title']
     duration = 1000*int(video['video']['duration']['seconds'])
-    summary = video['overview']['plot']
-    dir.Append(Function(VideoItem(PlayVideo, title=title, summary=summary, thumb=thumb, duration=duration), ext='mp4', videoId = videoId))
-  return dir
-
+    summary = video['overview']['plot'].replace('&#x22;','"').replace("&#x27;", "'")
+    directors = video['overview']['directors']
+    genres = video['overview']['genres']
+    oc.add(CreateTrailerObject(title, summary, thumb, duration, directors, videoId))
+  return oc
 
 ####################################################################################################
-# TODO: expand content by parsing TRAILERS, although the above seems to be their way ahead
-def Videos(sender):
-    dir = MediaContainer(viewGroup='Details')
-    return dir
-    
-####################################################################################################
-def PlayVideo(sender, videoId):
+def CreateTrailerObject(title, summary, thumb, duration, directors, videoId, include_container=False):
+  return MovieObject(
+    key = Callback(CreateTrailerObject, title=title, summary=summary, thumb=thumb, duration=duration, directors=directors, videoId=videoId, include_container=True),
+    rating_key = DETAILS_PAGE % videoId,
+    title = title,
+    summary = summary,
+    thumb = thumb,
+    duration = duration,
+    directors = directors,
+    items = [
+      MediaObject(
+	parts = [PartObject(key=Callback(PlayVideo, videoId=videoId, res='720'))],
+	container = Container.MP4,
+	audio_codec = AudioCodec.AAC,
+	video_codec = VideoCodec.H264,
+	video_resolution = '720',
+	audio_channels = 2,
+	optimized_for_streaming = True
+      ),
+      MediaObject(
+	parts = [PartObject(key=Callback(PlayVideo, videoId=videoId, res='480'))],
+	container = Container.MP4,
+	audio_codec = AudioCodec.AAC,
+	video_codec = VideoCodec.H264,
+	video_resolution = '480',
+	audio_channels = 2,
+	optimized_for_streaming = True
+      )
+    ]
+  )
+  
+def PlayVideo(videoId, res='720'):
+  if res == '720':
+    detailsUrl = DETAILS_PAGE % (videoId) + "?format=720p"
+  else:
     detailsUrl = DETAILS_PAGE % (videoId)
-    details = HTTP.Request(detailsUrl).content
-    index = details.find('mp4_h264')
-    start = details.find('http', index)
-    end = details.find("'", start)
-    videoUrl = details[start:end]
-    return Redirect(videoUrl)
+  details = HTTP.Request(detailsUrl).content
+  index = details.find('mp4_h264')
+  start = details.find('http', index)
+  end = details.find("'", start)
+  videoUrl = details[start:end]
+  return Redirect(videoUrl)
